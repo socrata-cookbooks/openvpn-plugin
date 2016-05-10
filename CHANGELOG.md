0b304e1e2f2974be47fcb37619b0c56deaad8a13
Jonathan Hartman <j@p4nt5.com>
2016-05-10 09:11:08 -0700

    Initial commit; a freshly generated cookbook

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+Openvpn Plugin Cookbook CHANGELOG
+=================================
+
+v0.0.1 (2016-05-10)
+-------------------
+- Development started
---
+++ metadata.rb
@@ -0,0 +1,14 @@
+# Encoding: UTF-8
+
+name 'openvpn-plugin'
+maintainer 'Jonathan Hartman'
+maintainer_email 'jonathan.hartman@socrata.com'
+license 'apache2'
+description 'Installs/configures OpenVPN plugins'
+long_description 'Installs/configures OpenVPN plugins'
+version '0.0.1'
+
+source_url 'https://github.com/socrata-cookbooks/openvpn-plugin'
+issues_url 'https://github.com/socrata-cookbooks/openvpn-plugin/issues'
+
+supports 'ubuntu'
